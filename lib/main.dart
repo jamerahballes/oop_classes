b2541720714c5d2cdc553d0c18de8d3a70840e0f
jamerahballes <jamerahballes@gmail.com>
2025-08-14 00:06:07 +0800

    feat(caji): add ClassName with properties and method

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'jamerah.dart';
+import 'caji.dart';
 
 void main() {
   var p1 = FlowerShop('FlowerShop', 'Seaside');
   p1.shop();
+
+  var p2 = Species('type', 'planet');
+  p2.kind();
 }
